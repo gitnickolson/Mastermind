693bb347c0e49786a108204f96e1578a1ba5a2ad
gitnickolson <nicknickolson@web.de>
2023-10-11 15:57:17 +0200

    Small code update - Made the code more readable - Divided the classes into seperated files - Added GitHub folder - Added gitignore to GitHub folder - Added README to github folder

--- pins.rb
+++
@@ -1,8 +0,0 @@
-class Pins
-  attr_reader :colors, :key_pegs
-
-  def initialize
-    @colors = ['🔴', '🔵', '🟡', '🟢', '🟣', '🟠']
-    @key_pegs = ['🟩', '🟪'] # Green = correct guess, Purple = close guess
-  end
-end
